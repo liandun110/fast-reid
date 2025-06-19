30634177d2ef1788dd47b2b4680488c8c6618ba1
zhbli <lizhenbang56@163.com>
2025-06-19 10:00:00 +0800

    重构demo.py,优化文件夹参数设置方式

--- demo/demo.py
+++ demo/demo.py
@@ -55,20 +55,14 @@ def get_parser():
         help='If use multiprocess for feature extraction.'
     )
     parser.add_argument(
-        "--input",
-        nargs="+",
-        help="A list of space separated input images; "
-             "or a single glob pattern such as 'directory/*.jpg'",
-    )
-    parser.add_argument(
-        "--output",
-        default='datasets/test_images',
-        help='path to save features'
+        "--seq-path",
+        default='datasets/yisuo/人脸追踪01/',
+        help="Base sequence path containing person_crops folder"
     )
     parser.add_argument(
         "--opts",
         help="Modify config options using the command-line 'KEY VALUE' pairs",
-        default=['MODEL.WEIGHTS', 'logs/market1501/mgn_R50-ibn/model_final.pth'],
+        default=['MODEL.WEIGHTS', 'datasets/market_mgn_R50-ibn.pth'],
         nargs=argparse.REMAINDER,
     )
     return parser
@@ -86,13 +80,32 @@ if __name__ == '__main__':
     cfg = setup_cfg(args)
     demo = FeatureExtractionDemo(cfg, parallel=args.parallel)
 
-    PathManager.mkdirs(args.output)
-    if args.input:
-        if PathManager.isdir(args.input[0]):
-            args.input = glob.glob(os.path.join(args.input[0], '*.jpg'))
-            assert args.input, "The input path(s) was not found"
-        for path in tqdm.tqdm(args.input):
-            img = cv2.imread(path)
-            feat = demo.run_on_image(img)
-            feat = postprocess(feat)
-            np.save(os.path.join(args.output, os.path.basename(path).split('.')[0] + '.npy'), feat)
+    # Set up paths
+    seq_path = args.seq_path
+    print('为视频{}提取特征……'.format(seq_path))
+    input_dir = os.path.join(seq_path, 'person_crops')
+    output_dir = os.path.join(seq_path, 'reid_features')
+    
+    PathManager.mkdirs(output_dir)
+    
+    # Get all image files from input directory
+    image_paths = glob.glob(os.path.join(input_dir, '*.jpg')) + \
+                 glob.glob(os.path.join(input_dir, '*.png')) + \
+                 glob.glob(os.path.join(input_dir, '*.jpeg'))
+    
+    if not image_paths:
+        raise ValueError(f"No images found in {input_dir}")
+    
+    # Process each image
+    for path in tqdm.tqdm(image_paths, desc="Extracting features"):
+        img = cv2.imread(path)
+        if img is None:
+            print(f"Warning: Could not read image {path}, skipping")
+            continue
+            
+        feat = demo.run_on_image(img)
+        feat = postprocess(feat)
+        
+        # Save feature with same name as input image but .npy extension
+        base_name = os.path.splitext(os.path.basename(path))[0]
+        np.save(os.path.join(output_dir, f"{base_name}.npy"), feat)
