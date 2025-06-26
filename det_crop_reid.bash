b9a688fa54b868008f75da7685d335404879daf1
zhbli <lizhenbang56@163.com>
2025-06-26 08:47:52 +0800

    重要进展:将mgn特征换成bot特征,解决了不同视频同一身份得分接近0的问题.

--- det_crop_reid.bash
+++ det_crop_reid.bash
@@ -1,5 +1,5 @@
 # 依次对图像序列执行检测行人、裁剪行人、提取reid特征三步操作。
-SEQPATH=datasets/yisuo/人脸追踪03
+SEQPATH=datasets/yisuo/人脸追踪02
 python datasets/person_detect.py --seq_path=$SEQPATH
 python datasets/crop_person.py --seq_path=$SEQPATH
-python demo/demo.py --seq_path=$SEQPATH
+python demo/demo.py --config-file configs/DukeMTMC/bagtricks_R101-ibn.yml --seq_path=$SEQPATH --opts MODEL.WEIGHTS datasets/duke_bot_R101-ibn.pth
